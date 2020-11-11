253cd53005ef772a059b8c9480c715ce46fff495
iii <miselrkba@gmail.com>
2020-11-11 22:07:33 +0100

    added colorArray and boxes with hex color nums

--- src/ColorBoxes.js
+++ src/ColorBoxes.js
@@ -1,25 +1,11 @@
 import React from "react";
+import { colorArray } from "./colorArray";
 
 function ColorBoxes() {
-  const numbers = [...Array(32).keys()];
-
-  // const evenNumbers = numbers.map((item) => item % 2 === 0);
-  // const oddNumbers = numbers.map((item) => item % 2 !== 0);
-  const primeNumbers = numbers.filter(isPrime);
-
-  function isPrime(num) {
-    for (let start = 2; num > start; start++) {
-      if (num % start === 0) {
-        return false;
-      }
-    }
-    return num > 1;
-  }
-
   return (
     <div className="grid">
-      {numbers.map((item) => (
-        <div className={`grid-box ${(item % 2 === 0 ? 'even' : 'odd')} ${isPrime(item) ? 'prime' : null} `} key={item}>
+      {colorArray.map((item) => (
+        <div className="grid-box" style={{ background: `${item}` }} key={item}>
           {item}
         </div>
       ))}
