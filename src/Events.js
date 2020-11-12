b87b608fd89a27e9841990199912f7f1473aa206
iii <miselrkba@gmail.com>
2020-11-12 19:28:58 +0100

    first commit - moving box

--- src/Events.js
+++ src/Events.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { useState } from "react";
 
 function Events() {
+  const [left, setLeft] = useState(1);
+  const [right, setRight] = useState(1);
+  const [top, setTop] = useState(1);
+  const [bottom, setBottom] = useState(1);
+
+  const randomNumber = Math.random() * 2;
+  const randomNumber1 = Math.random() * 15;
+  const randomNumber2 = Math.random() * 15;
+  const randomNumber3 = Math.random() * 10;
+
+  const handleMouseMove = () => {
+    setLeft(randomNumber);
+    setRight(randomNumber1);
+    setTop(randomNumber2);
+    setBottom(randomNumber3);
+  };
+
+  console.log(left, right);
+
   return (
     <div>
-      <div className='box'> 30 Days of React</div>
+      <div
+        onMouseMove={handleMouseMove}
+        className="box"
+        style={{
+          position: "relative",
+          left: `${left}em`,
+          right: `${right}em`,
+          top: `${top}em`,
+          bottom: `${bottom}em`,
+        }}
+      >
+        {" "}
+        30 Days of React
+      </div>
     </div>
   );
 }
