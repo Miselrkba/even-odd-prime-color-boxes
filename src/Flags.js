6baf455b8e6e0fc004bc722812d393f625a19851
iii <miselrkba@gmail.com>
2020-11-19 20:41:21 +0100

    basic html setup

--- src/Flags.js
+++ src/Flags.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Flags() {
-    return (
-        <div>
-            flags
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <h1>30 days of React</h1>
+        <h5>Getting started with React</h5>
+        <p>JavaScript Library</p>
+        <p>Michael Angelo</p>
+        <p>Nov 19/11/2020</p>
+        <p>Select a country for your holiday</p>
+      </div>
+      <div>Card</div>
+      <button>Select Country</button>
+      <div>Copyright2020</div>
+    </div>
+  );
 }
 
-export default Flags
+export default Flags;
